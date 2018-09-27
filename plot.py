6f1f171ef1f455b510df856807ac06698d7c5438
Kevin <kevin@kehei.de>
2018-09-27 15:36:35 +0200

    Show more time

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
+""" This will create a very nice plot
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.linspace(0, 1, 1000)
+t = np.linspace(0, 3, 1000)
 
 U = np.sin(5 * np.pi * t)
 
