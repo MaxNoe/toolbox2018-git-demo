1be8aaf85e845dbd03f1ddd77ec0058b1ca71973
Kevin <kevin@kehei.de>
2018-09-27 15:41:06 +0200

    Add label

--- plot.py
+++ plot.py
@@ -7,7 +7,8 @@ t = np.linspace(0, 3, 1000)
 
 U = np.sin(5 * np.pi * t)
 
-plt.plot(t, U)
+plt.plot(t, U, label="Spannung")
 plt.xlabel('t / s')
 plt.ylabel('U / V')
+plt.legend()
 plt.savefig('build/plot.pdf')
