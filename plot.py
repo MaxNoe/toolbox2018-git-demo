5d0fe91b9f3e02db6ffb12286d8c9901c000908c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-09-27 15:31:30 +0200

    Make plot more interesting
    * plot voltage vs time
    * omega = 5 * π

--- plot.py
+++ plot.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+t = np.linspace(0, 1, 1000)
 
-plt.plot([1, 2, 3])
+U = np.sin(5 * np.pi * t)
+
+plt.plot(t, U)
+plt.xlabel('t / s')
+plt.ylabel('U / V')
 plt.savefig('build/plot.pdf')
